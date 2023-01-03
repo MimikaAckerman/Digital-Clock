389a348b3a8081ee8702a5355b441f9c984fb6e6
Mimika Ackerman <emily.herreragomez@gmail.com>
2023-01-03 01:02:59 +0100

    desing

--- app.js
+++ app.js
@@ -1,65 +1,31 @@
-setInterval(showTime,1000);
-function showTime(){
-    let time = new Date();
-    let hour = time.getHours();
-    let min = time.getMinutes();
-    let sec = time.getSeconds();
-    am_pm = "AM";
+function clockTime() {
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
 
-    if(hour > 12){
-        hour -= 12;
-        am_pm = 'PM';
-    }
-    if(hour == 0){
-        hr = 12;
-        am_pm = "AM";
-    }
-    hour = hour < 10 ? "0" + hour : hour;
-    min = min < 10 ? "0" + min : min;
-    sec = sec < 10 ? "0" + sec : sec;
+  //now we need a created a conditions for the different events
 
-let currentTime = hour + ":"
-            + min + ":" + sec + am_pm;
- 
-    document.getElementById("clock")
-            .innerHTML = currentTime;
+  //first condition is hours is  == to 0 the valor session is 12
+  if (hh == 0) {
+    hh = 12;
+  }
 
+  if (hh > 12) {
+    hh = hh - 12;
+    session = "PM";
+  }
 
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
+  ss = ss < 10 ? "0" + ss : ss;
+
+  let time = hh + ":" + mm + ":" + ss + " " + session;
+
+  document.getElementById("clock").innerHTML = time;
+  let t = setTimeout(function () {
+    clockTime();
+  }, 1000);
 }
-
-showTime();
-
-
-
-
-
-
-
-
-/* function showTime(){
-    const date = new Date();
-    const h = date.getHours();
-    const m = date.getMinutes();
-    const s = date.getSeconds();
-
-    const session = "AM";
-
-    if(h == 0){
-        h = 12;
-    }
-    if(h > 12){
-        h= h -12;
-        session = "PM";
-    }
-    h = (h <10) ? "0" + h : h;
-    m = (m <10) ? "0" + m : m;
-    s = (s <10) ? "0" + s : s;
-
-    const time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-
-    setTimeout(showTime,1000);
-
-}
-showTime(); */
+clockTime();
